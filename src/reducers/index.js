255fa352a1f6b66833b022ed407d938c69dcfcc0
Frontend4Tribe <thecodingisanart@gmail.com>
2021-07-23 12:11:14 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+// action = {
+//  type: 'CREATE_EVENT',
+//  title: 'TESTイベントのお知らせ'
+//  body: '2021年にTESTイベントを開催します。つきましては、、、、、、'
+// }
+//
+// #before
+// state = []
+//
+// #after
+// state = [
+//   {
+//    id: 1,
+//    title: 'TESTイベントのお知らせ',
+//    body: '2021年にTESTイベントを開催します。つきましては、、、、、、'
+//   }
+// ]
+//
+// #before
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+//
+// #after
+// state = [
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+//   {
+//    id: 4,
+//    title: 'TESTイベントのお知らせ',
+//    body: '2021年にTESTイベントを開催します。つきましては、、、、、、'
+//   }
+// ]
+const events = (state = [], action) => {
+    switch(action.type) {
+        case 'CREATE_EVENT':
+            const event = { title: action.title, body: action.body }
+            const length = state.length
+            const id = length === 0 ? 1 : state[length - 1].id + 1
+            return [...state, { id, ...event }]
+        case 'DELETE_EVENT':
+            return state
+        case 'DELETE_ALL_EVENT':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
